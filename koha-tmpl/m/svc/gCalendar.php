e2c9d66a34e776e0f7dd5b1b01ed41455ddd3ed2
Bibliothèques de l'uiversité Rennes 2 <bibliotheques@univ-rennes2.fr>
2013-01-22 22:58:57 +0100

    Update koha-tmpl/m/svc/gCalendar.php

--- koha-tmpl/m/svc/gCalendar.php
+++ koha-tmpl/m/svc/gCalendar.php
@@ -1,20 +1,20 @@
 <?php
-
+require_once 'config.php';
 $userid = $_GET['userid'];
 $currentDay = date('Y-m-d',$_GET['time']);
 $nextDay = date('Y-m-d',$_GET['time']+86400);
 
 $opts = array(
     'http' => array (
-        'proxy'=>'tcp://wwwcache.uhb.fr:8080',
+        'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
         'request_fulluri' => true
     ),
     'https' => array (
-        'proxy'=>'tcp://wwwcache.uhb.fr:8080',
+        'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
         'request_fulluri' => true
     )
 );
-// Création du contexte de transaction
+// Crï¿½ation du contexte de transaction
 $ctx = stream_context_create($opts);    
 
 $url = 'http://www.google.com/calendar/feeds/'.$userid.'/public/full?alt=json-in-script&start-min='.$currentDay.'&start-max='.$nextDay.'&singleevents=true&orderBy=startTime&sortorder=ascending&callback=t';
