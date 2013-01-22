d6f88a5d1d38a9231fcb65f62df185cccadffdd1
Bibliothèques de l'uiversité Rennes 2 <bibliotheques@univ-rennes2.fr>
2013-01-22 23:01:22 +0100

    Update koha-tmpl/m/svc/gCalendar.php

--- koha-tmpl/m/svc/gCalendar.php
+++ koha-tmpl/m/svc/gCalendar.php
@@ -3,22 +3,28 @@ require_once 'config.php';
 $userid = $_GET['userid'];
 $currentDay = date('Y-m-d',$_GET['time']);
 $nextDay = date('Y-m-d',$_GET['time']+86400);
-
-$opts = array(
-    'http' => array (
-        'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
-        'request_fulluri' => true
-    ),
-    'https' => array (
-        'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
-        'request_fulluri' => true
-    )
-);
-// Cr�ation du contexte de transaction
-$ctx = stream_context_create($opts);    
-
 $url = 'http://www.google.com/calendar/feeds/'.$userid.'/public/full?alt=json-in-script&start-min='.$currentDay.'&start-max='.$nextDay.'&singleevents=true&orderBy=startTime&sortorder=ascending&callback=t';
-$feed = file_get_contents($url,false,$ctx);
+
+//Proxy 
+    if(!is_null($proxy_server)){
+        $opts = array(
+            'http' => array (
+                'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
+                'request_fulluri' => true
+            ),
+            'https' => array (
+                'proxy'=>'tcp://' . $proxy_server . ':' . $proxy_port. '',
+                'request_fulluri' => true
+            )
+        );
+        // Création du contexte de transaction
+        $ctx = stream_context_create($opts);    
+        $feed = file_get_contents($url,false,$ctx);
+     }
+     else { 
+         $feed = file_get_contents($url);
+     }  
+
 
 header('Content-Type: text/javascript; charset=utf8');
 header('Access-Control-Allow-Methods: GET, POST');
